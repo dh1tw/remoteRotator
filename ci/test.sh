c6904005c20692d733cc3c48563582019f8d2607
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2017-09-05 00:30:32 +0200

    run tests with race detector and verbose output

--- ci/test.sh
+++ ci/test.sh
@@ -2,11 +2,11 @@
 
 
 if [ "${GIMME_OS}" = "darwin" ] && [ "${GIMME_ARCH}" = "amd64" ]; then
-    go test ./...
+    go test -v -race ./...
 fi
 
 if [ "${GIMME_OS}" = "linux" ] && [ "${GIMME_ARCH}" = "amd64" ]; then
     $HOME/gopath/bin/goveralls -service=travis-ci
-    go test ./...
+    go test -v -race ./...
 fi
 
