9b046df0e91791a1d7c006d178fcfabec9a49264
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2017-12-08 03:31:54 +0100

    changed mDNS domain due to problems on windows

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -19,6 +19,44 @@ type RotatorMdnsEntry struct {
 
 // LookupRotators will perform an mDNS query are lookup all available
 // rotators on the network.
+// func LookupRotators() ([]RotatorMdnsEntry, error) {
+// 	entriesCh := make(chan *mdns.ServiceEntry, 100)
+
+// 	// rotators := []*mdns.ServiceEntry{}
+// 	rotators := []RotatorMdnsEntry{}
+
+// 	go func() {
+// 		for entry := range entriesCh {
+
+// 			// ignore if not rotators.shackbus.local
+// 			if !strings.Contains(entry.Name, "rotators.shackbus.local") {
+// 				continue
+// 			}
+
+// 			name := strings.TrimSuffix(entry.Name, ".rotators.shackbus.local.")
+// 			// replace '\' (escaping backslashes)
+// 			name = strings.Replace(name, "\x5c", "", -1)
+
+// 			r := RotatorMdnsEntry{
+// 				Name:   name,
+// 				URL:    strings.TrimSuffix(entry.Name, "."),
+// 				Host:   strings.TrimSuffix(entry.Host, "."),
+// 				AddrV4: entry.AddrV4,
+// 				AddrV6: entry.AddrV6,
+// 				Port:   entry.Port,
+// 			}
+// 			rotators = append(rotators, r)
+// 		}
+// 	}()
+
+// 	// perform mDNS lookup on all interfaces (default) and wait for 1 sec
+// 	// for responses (default)
+// 	mdns.Lookup("rotators.shackbus", entriesCh)
+
+// 	close(entriesCh)
+// 	return rotators, nil
+// }
+
 func LookupRotators() ([]RotatorMdnsEntry, error) {
 	entriesCh := make(chan *mdns.ServiceEntry, 100)
 
@@ -29,11 +67,11 @@ func LookupRotators() ([]RotatorMdnsEntry, error) {
 		for entry := range entriesCh {
 
 			// ignore if not rotators.shackbus.local
-			if !strings.Contains(entry.Name, "rotators.shackbus.local") {
+			if !strings.Contains(entry.Name, "_rotators._shackbus._tcp.local") {
 				continue
 			}
 
-			name := strings.TrimSuffix(entry.Name, ".rotators.shackbus.local.")
+			name := strings.TrimSuffix(entry.Name, "._rotators._shackbus._tcp.local.")
 			// replace '\' (escaping backslashes)
 			name = strings.Replace(name, "\x5c", "", -1)
 
@@ -51,7 +89,25 @@ func LookupRotators() ([]RotatorMdnsEntry, error) {
 
 	// perform mDNS lookup on all interfaces (default) and wait for 1 sec
 	// for responses (default)
-	mdns.Lookup("rotators.shackbus", entriesCh)
+
+	// ifis, err := net.Interfaces()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// for _, ifi := range ifis {
+	// 	qm := mdns.QueryParam{
+	// 		Service:   "_rotators._shackbus",
+	// 		Interface: &ifi,
+	// 		Entries:   entriesCh,
+	// 	}
+
+	// 	if err := mdns.Query(&qm); err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// }
+
+	mdns.Lookup("_rotators._shackbus._tcp", entriesCh)
 
 	close(entriesCh)
 	return rotators, nil
