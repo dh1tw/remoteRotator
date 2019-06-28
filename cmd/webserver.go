c63d51f3ad91b8a20f8ff177e5c062aebb39c633
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2019-06-28 23:23:06 +0200

    updated micro selector dependency

--- cmd/webserver.go
+++ cmd/webserver.go
@@ -11,8 +11,8 @@ import (
 
 	"github.com/micro/go-micro/broker"
 	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/client/selector/static"
 	"github.com/micro/go-micro/registry"
-	"github.com/micro/go-micro/selector/static"
 	"github.com/micro/go-micro/transport"
 	natsBroker "github.com/micro/go-plugins/broker/nats"
 	natsReg "github.com/micro/go-plugins/registry/nats"
