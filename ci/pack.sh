30baa4dfb7ae5f4a8617e1d735df8452d8eac3f8
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2019-11-25 03:09:52 +0100

    removed obsolete CI files

--- ci/pack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-if [ "${GIMME_OS}" = "windows" ]; then
-    zip remoteRotator-v$TRAVIS_TAG-$GIMME_OS-$GIMME_ARCH.zip remoteRotator.exe
-else 
-    tar -cvzf remoteRotator-v$TRAVIS_TAG-$GIMME_OS-$GIMME_ARCH.tar.gz remoteRotator
-fi
