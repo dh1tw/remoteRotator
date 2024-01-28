10e48448e937edf9b24779dd3889855a8c7c0958
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2024-01-28 19:03:34 +0100

    autogenerate the copyright year

--- cmd/version.go
+++ cmd/version.go
@@ -28,7 +28,7 @@ func init() {
 
 func printVersion() {
 	buildDate := time.Now().Format(time.RFC3339)
-	fmt.Println("copyright Tobias Wellnitz, DH1TW, 2020")
+	fmt.Printf("copyright Tobias Wellnitz, DH1TW (%d)\n", time.Now().Year())
 	fmt.Printf("remoteRotator Version: %s, %s/%s, BuildDate: %s, Commit: %s\n",
 		version, runtime.GOOS, runtime.GOARCH, buildDate, commitHash)
 }
